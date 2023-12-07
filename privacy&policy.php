5a3735b62ef9d65405b1a7fa088deb47866f5da2
Avishka2023 <153220441+Avishka2023@users.noreply.github.com>
2023-12-07 23:40:19 +0530

    Add files via upload

---
+++ privacy&policy.php
@@ -0,0 +1,151 @@
+<head>
+
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Meta Tags -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="shortcut icon" href="images/favicon.ico">
+
+<!--	Fonts   -->
+<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
+
+<!--	Css Link   -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="css/layerslider.css">
+<link rel="stylesheet" type="text/css" href="css/color.css">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/login.css">
+<!--	Title   -->
+
+</head>
+<body>
+
+<div id="page-wrapper">
+    <div class="row"> 
+        <!--	Header start  -->
+		<?php include("include/header.php");?>
+        <!--	Header end  -->
+
+        <!-----  Our Services  ---->
+        <div class="full-row bg-gray">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h2 class="text-secondary double-down-line text-center mb-5">Privacy & Policy</h2></div>
+                        <p class="text-center">Last updated: 10/30/2023 <br>
+                            Mr.LandLoard we operates www.mrlandloard.lk (the "Site"). This page informs you of our policies regarding the collection, use, and disclosure of Personal Information we receive from users of the Site.
+                            <br>We use your Personal Information only for providing and improving the Site. By using the Site, you agree to the collection and use of information in accordance with this policy.</p>
+                </div>
+                <hr>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#">Information Collection and Use</a></h5><hr>
+                                <p>While using our Site, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you. Personally identifiable information may include, 
+                                    but is not limited to your name, email address, and phone number ("Personal Information").</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#">Log Data</a></h5><hr>
+                                <p>Like many site operators, we collect information that your browser sends whenever you visit our Site ("Log Data"). This Log Data may include information such as your computer's Internet Protocol ("IP") address, browser type, browser version, the pages of our Site that you visit, 
+                                    the time and date of your visit, the time spent on those pages, and other statistics.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#">Cookies</a></h5><hr>
+                                <p>Cookies are files with small amounts of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your computer's hard drive.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#">Security</a></h5><hr>
+                                <p>The security of your Personal Information is important to us, but remember that no method of transmission over the Internet or method of electronic storage is 100% secure. 
+                                    While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#">Changes to this Privacy Policy</a></h5><hr>
+                                <p>This Privacy Policy is effective as of [Date] and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page.
+                                    We reserve the right to update or change our Privacy Policy at any time, and you should check this Privacy Policy periodically. Your continued use of the Service after we post any modifications to the 
+                                    Privacy Policy on this page will constitute your acknowledgment of the modifications and your consent to abide and be bound by the modified Privacy Policy.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#">Contact Us</a></h5><hr>
+                                <p>If you have any questions about this Privacy Policy, please contact us at at +94 771236789.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                    <p>Mr.LandLoard, www.mrlandloard.lk, 10/30/2023, and +94 771236789 with your actual website information. Additionally, consult with a legal professional to ensure that your Privacy Policy complies with relevant laws and regulations.</p>
+            </div>
+        </div>
+		<!-----  Our Services  ---->
+
+        <!--	Footer   start-->
+		<?php include("include/footer.php");?>
+		<!--	Footer   start-->
+        
+        <!-- Scroll to top --> 
+        <a href="#" class="bg-success text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
+        <!-- End Scroll To top --> 
+    </div>
+</div>
+
+<!--	Js Link   --> 
+<script src="js/jquery.min.js"></script> 
+
+<!--jQuery Layer Slider --> 
+<script src="js/greensock.js"></script> 
+<script src="js/layerslider.transitions.js"></script> 
+<script src="js/layerslider.kreaturamedia.jquery.js"></script> 
+
+<!--jQuery Layer Slider --> 
+<script src="js/popper.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/tmpl.js"></script> 
+<script src="js/jquery.dependClass-0.1.js"></script> 
+<script src="js/draggable-0.1.js"></script> 
+<script src="js/jquery.slider.js"></script> 
+<script src="js/wow.js"></script> 
+<script src="js/YouTubePopUp.jquery.js"></script> 
+<script src="js/validate.js"></script> 
+<script src="js/jquery.cookie.js"></script> 
+<script src="js/custom.js"></script>
+
+</body>
+</head>
---
+++ terms&conditions.php
@@ -0,0 +1,195 @@
+<head>
+
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Meta Tags -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="shortcut icon" href="images/favicon.ico">
+
+<!--	Fonts   -->
+<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
+
+<!--	Css Link   -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="css/layerslider.css">
+<link rel="stylesheet" type="text/css" href="css/color.css">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/login.css">
+<!--	Title   -->
+
+</head>
+<body>
+
+<div id="page-wrapper">
+    <div class="row"> 
+        <!--	Header start  -->
+		<?php include("include/header.php");?>
+        <!--	Header end  -->
+
+        <!-----  Our Services  ---->
+        <div class="full-row bg-gray">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h2 class="text-secondary double-down-line text-center mb-5">Terms & Conditions</h2></div>
+                        <p class="text-center">These Terms and Conditions govern your use of the Mr.LandLoard website www.mrlandloard.lk and any related services, features, content, or applications (collectively referred to as the "Services"). 
+                            Please read these Terms carefully before using the Website. By accessing or using the Website and its Services, you agree to be bound by these Terms. 
+                            If you do not agree with any part of these Terms, you may not use the Website or its Services.</p>
+                </div>
+                <hr>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#">Acceptance of Terms</a></h5><hr>
+                                <p>By using the Website, you affirm that you are at least 18 years of age and capable of forming a binding contract. If you are using the Website on behalf of an organization, 
+                                    you represent and warrant that you have the authority to bind that organization and that your use of the Website is subject to these Terms.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#">Changes to Terms</a></h5><hr>
+                                <p>We reserve the right to change, modify, or revise these Terms at any time, with or without notice. Your continued use of the Website after any such changes constitutes your acceptance of the new Terms.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#">User Registration</a></h5><hr>
+                                <p>Some areas of the Website may require user registration. When creating an account, you agree to provide accurate and complete information. You are solely responsible for maintaining the confidentiality of your account and password. 
+                                    You agree to accept responsibility for all activities that occur under your account.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#">User Responsibilities</a></h5><hr>
+                                <p>
+                                    <ul>
+                                        <li>You agree to use the Website and its Services in compliance with all applicable laws and regulations.</li>
+                                        <li>You agree not to engage in any behavior that is harmful, illegal, or unethical.</li>
+                                        <li>You agree not to attempt to gain unauthorized access to the Website, other users' accounts, or any other computer systems.</li>
+                                        <li>You agree not to post, upload, or share any content that is defamatory, illegal, or infringing on the rights of others.</li>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#">Intellectual Property</a></h5><hr>
+                                <p>All content on the Website, including text, images, logos, and software, is protected by copyright and other intellectual property laws. You may not use, reproduce, 
+                                    or distribute any content from the Website without the written consent of the owner.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#"> Limitation of Liability</a></h5><hr>
+                                <p>The Website and its Services are provided "as is" and without any warranties. We shall not be liable for any direct, indirect, special, incidental, consequential, 
+                                    or punitive damages arising from your use of the Website or any errors or omissions in its content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#"> Privacy</a></h5><hr>
+                                <p>Your use of the Website is also governed by our Privacy Policy, which can be found </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#">Termination</a></h5><hr>
+                                <p>We reserve the right to terminate your access to the Website and its Services without notice, for any reason, including a breach of these Terms.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#">Contact Information</a></h5><hr>
+                                <p>If you have any questions or concerns about these Terms or the Website, please contact us at +94 771236789.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-box-one">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="p-4 hover-bg-white hover-shadow rounded mb-4 transation-3s"> 
+                                <h5 class="text-secondary hover-text-success py-3 m-0"><a href="#">Governing Law</a></h5><hr>
+                                <p>These Terms are governed by the laws of Mr.LandLoard, and any disputes shall be subject to the exclusive jurisdiction of the courts of Mr.LandLoard.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+		<!-----  Our Services  ---->
+
+        <!--	Footer   start-->
+		<?php include("include/footer.php");?>
+		<!--	Footer   start-->
+        
+        <!-- Scroll to top --> 
+        <a href="#" class="bg-success text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
+        <!-- End Scroll To top --> 
+    </div>
+</div>
+
+<!--	Js Link   --> 
+<script src="js/jquery.min.js"></script> 
+
+<!--jQuery Layer Slider --> 
+<script src="js/greensock.js"></script> 
+<script src="js/layerslider.transitions.js"></script> 
+<script src="js/layerslider.kreaturamedia.jquery.js"></script> 
+
+<!--jQuery Layer Slider --> 
+<script src="js/popper.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/tmpl.js"></script> 
+<script src="js/jquery.dependClass-0.1.js"></script> 
+<script src="js/draggable-0.1.js"></script> 
+<script src="js/jquery.slider.js"></script> 
+<script src="js/wow.js"></script> 
+<script src="js/YouTubePopUp.jquery.js"></script> 
+<script src="js/validate.js"></script> 
+<script src="js/jquery.cookie.js"></script> 
+<script src="js/custom.js"></script>
+
+</body>
+</head>
